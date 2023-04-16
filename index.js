66ecce096abdd2e150bf0618b5a48df5cb1ce2c7
AbitBrookish <brooksjohnson25@gmail.com>
2023-04-16 16:23:54 -0400

    create function, set variables, add event

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import quotes from "./dummyAPI.js";
 
+let button = document.getElementById('btn');
+let display = document.getElementById('display');
+let header = document.getElementById('title');
+let p1 = document.getElementById('text');
+let p2 = document.getElementById('author');
 // you can access individual quotes from the quotes array like this:
-console.log(quotes[0].text); // outputs the text of the quote
-console.log(quotes[0].author); // outputs the author of the quote
 
 
 // your job is to use DOM manipulation to display a random quote in the index.html page that is retrieved from the quotes array
+
+// functions
+function randomQuote() {
+  let index = quotes.length;
+  let quote = quotes[Math.floor(Math.random() * index)];
+  p1.innerHTML = quote.text;
+  p2.innerHTML = quote.author;
+}
+
+button.addEventListener('click', randomQuote);
